93e5af2ed53506bb02d74b76ef0e58ce8868f006
hailthefish <hailthefish@users.noreply.github.com>
2019-09-13 00:25:56 -0700

    quick and dirty main loop
    nothing actually happens but it runs in a loop and updates at a set time

--- Program.cs
+++ Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace yacsmu
 {
     class Program
     {
-        static void Main(string[] args)
+        private const long MAIN_TICKRATE = 250; // milliseconds, how often the main loop runs.
+
+        private static void Startup()
         {
             Console.WriteLine("Loading Configuration...");
             Config.ConfigureSettings();
             Console.WriteLine("Configuration Loaded.");
+
+
+            // Last thing in startup
             Console.WriteLine("Preparing to start server on port " + Config.configuration["server:port"]);
+            Console.ReadLine();
+        }
+
+        private static void MainLoop()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            Console.WriteLine("Running.");
+            bool running = true;
+            int counter = 0;
+            while (running)
+            {
+                counter++;
+
+                // Background/connection stuff happens here?
+
+                if (timer.ElapsedMilliseconds >= MAIN_TICKRATE)
+                {
+
+                    // Game Update Stuff Happens Here
+
+
+                    Console.WriteLine("Tick: " + timer.ElapsedMilliseconds + ":" + counter);
+                    timer.Restart();
+                    counter = 0;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Startup();
+
+            MainLoop();
+
+            //Shutdown goes here
         }
     }
 }
--- Server.cs
+++ Server.cs
@@ -8,18 +8,24 @@ namespace yacsmu
 {
     internal static class Server
     {
+        internal const string LINE_END = "\r\n";
+
+        static TcpListener listener;
+
         internal static void SetupServer()
         {
             try
             {
                 int port = int.Parse(Config.configuration["Server:port"]);
-                var server = new TcpListener(IPAddress.Any, port);
+                listener = new TcpListener(IPAddress.Any, port);
             }
             catch (Exception)
             {
                 throw;
             }
-            
+
+            listener.Start();
         }
+
     }
 }
