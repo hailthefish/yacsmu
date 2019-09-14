7eaec716737ebad8215b4508720ea09a18a48f96
hailthefish <hailthefish@users.noreply.github.com>
2019-09-14 16:53:45 -0700

    moved all the telnet negotiation definitions to Def.cs

--- Def.cs
+++ Def.cs
@@ -5,5 +5,26 @@
         internal const int OUTBUF_SIZE = 2048;
 
         internal const string NEWLINE = "\r\n";
+
+        //Telnet Negotiation
+        internal const byte IAC = 0xFF;
+        internal const byte DO = 0xFD;
+        internal const byte DONT = 0xFE;
+        internal const byte WILL = 0xFB;
+        internal const byte WONT = 0xFC;
+        internal const byte NOP = 0xF1;
+        //Subnegotiation
+        internal const byte SB = 0xFA;
+        internal const byte SE = 0xF0;
+        internal const byte IS = 0x00;
+        internal const byte SEND = 0x01;
+        //Options
+        internal const byte SGA = 0x03; // Suppress Go-ahead
+        internal const byte RFC = 0x21; // Remote Flow Control
+        internal const byte NAWS = 0x1F; // Negotiate about window size
+        internal const byte ECHO = 0x01;
+        internal const byte TYPE = 0x18; // Terminal type
+        //Misc
+        internal const byte GA = 0xF9; //Telnet go-ahead
     }
 }
--- Server.cs
+++ Server.cs
@@ -91,25 +91,6 @@ namespace yacsmu
         }
 
 
-        //Telnet Negotiation
-        private const byte IAC = 0xFF;
-        private const byte DO = 0xFD;
-        private const byte DONT = 0xFE;
-        private const byte WILL = 0xFB;
-        private const byte WONT = 0xFC;
-        private const byte NOP = 0xF1;
-        //Subnegotiation
-        private const byte SB = 0xFA;
-        private const byte SE = 0xF0;
-        private const byte IS = 0x00;
-        private const byte SEND = 0x01;
-        //Options
-        private const byte SGA = 0x03; // Suppress Go-ahead
-        private const byte RFC = 0x21; // Remote Flow Control
-        private const byte NAWS = 0x1F; // Negotiate about window size
-        private const byte ECHO = 0x01;
-        private const byte TYPE = 0x18; // Terminal type
-        //Misc
-        private const byte GA = 0xF9; //Telnet go-ahead
+
     }
 }
